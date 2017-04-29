c3138e0175715bf84d4395c15e3f414c71a1af4b
maryito <mtorres@uip.edu.pa>
2017-04-29 18:14:51 -0500

    Publicaciones

--- client/api/api.js
+++ client/api/api.js
@@ -1,20 +1,20 @@
 import { Template } from 'meteor/templating';
 import './api.html';
 
-Template.api.onCreated( function () {
-  Session.set('+Api', 5)
+Template.api.onCreated(function () {
+  Session.set('+Api', 30)
 })
 
 Template.api.helpers({
- data() {
-   const aum = Session.get('+Api')
-   return Api.find({}, { limit: aum});
- }
+  data() {
+    const aum = Session.get('+Api')
+    return Api.find({}, { limit: aum });
+  }
 });
-Template.api.events({ 
-  'click #apiMas': function(event, template) { 
-     event.preventDefault();
-     const x = Session.get('+Api') + 10;
-     Session.set('+Api', x)
-  } 
+Template.api.events({
+  'click #apiMas': function (event, template) {
+    event.preventDefault();
+    const x = Session.get('+Api') + 10;
+    Session.set('+Api', x)
+  }
 });

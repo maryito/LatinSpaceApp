5ef96d12a4df876f246f0230af0022ba15e36d43
maryito <m_torres@uip.edu.pa>
2017-04-29 10:52:53 -0500

    api rendimiento

--- client/api/api.js
+++ client/api/api.js
@@ -1,9 +1,20 @@
 import { Template } from 'meteor/templating';
 import './api.html';
 
+Template.api.onCreated( function () {
+  Session.set('+Api', 5)
+})
 
 Template.api.helpers({
  data() {
-   return Api.find();
+   const aum = Session.get('+Api')
+   return Api.find({}, { limit: aum});
  }
+});
+Template.api.events({ 
+  'click #apiMas': function(event, template) { 
+     event.preventDefault();
+     const x = Session.get('+Api') + 10;
+     Session.set('+Api', x)
+  } 
 });

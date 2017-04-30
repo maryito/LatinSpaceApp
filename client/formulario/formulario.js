c5a28790fd92a40bffacad26447b4b1ea709b844
Luis Mora <luismora1997@gmail.com>
2017-04-30 09:32:37 -0500

    Subiendo varios cambios

--- client/formulario/formulario.js
+++ client/formulario/formulario.js
@@ -1,11 +1,26 @@
 Template.formularios.onCreated( function(){
     Session.set('FormularioListo', false);
-      GoogleMaps.load({
+    /*  GoogleMaps.load({
         key: "AIzaSyCTmQZ_IJoqteedF6XDFjdsaCgaSxDnhik",
-        template: "places" 
-     });
+        template: "places"
+     }); */
 })
 
+Template.formularios.onRendered( function(){
+  L.Icon.Default.imagePath = '/packages/bevanhunt_leaflet/images/';
+  var mapUrl = 'http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png';
+  var mapTiles = L.tileLayer(mapUrl);
+  var map = L.map('formmap', {
+    zoom: 5,
+    center: [0,0],
+    scrollWheelZoom: true,
+    layers: [mapTiles]
+  });
+  map.spin(true);
+  map._layersMinZoom=3;
+  map.setMaxBounds([[84.67351256610522, -174.0234375], [-64.77413, 15.82031]]);
+  map.spin(false);
+})
 
 Template.formularios.helpers({
    ReadyForm() {
@@ -59,7 +74,7 @@ AutoForm.hooks({
       // Called when any submit operation succeeds
   onSuccess: function(formType, result) {
       console.log("Agregando reporte ", result )
-    
+
   },
   // Called when any submit operation fails
   onError: function(formType, error) {
